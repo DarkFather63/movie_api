5400e6c3acc94dd024a0cc3624b276373637b8a6
DarkFather63 <erynlcraig@gmail.com>
2022-06-01 09:43:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 
