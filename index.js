979e23950f653f3fa994796a058fee8d2048a59a
DarkFather63 <erynlcraig@gmail.com>
2022-06-01 11:00:31 -0700

    Added heroku to allowed origins

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://eryn-moviedb.herokuapp.com/movies'];
 
 //Disallow cross origin access
 app.use(cors({
