a9c9e12d7ca7da976144722af1d56bc788dc0639
DarkFather63 <erynlcraig@gmail.com>
2022-06-02 12:16:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ app.get('/director/:Name', (req, res) => {
 
 
 // READ: GET all users
-app.get('/users', (req, res) => {
+app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.find()
     .then((users) => {
       res.status(201).json(users);
